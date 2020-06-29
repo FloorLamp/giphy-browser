a6264647ef395268d50f2013cd3eb0ba4718bba2
Norton Wang <nortantivirus@gmail.com>
2020-06-29 09:38:10 +0700

    Remove unused pages

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,3 @@
-import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
@@ -16,17 +15,7 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+      <input type="text" placeholder="Search GIFs..." />
     </div>
   </header>
 )
